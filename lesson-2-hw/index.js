a6d3bbaff63f77807dd2d2a3656a3ee0fd8d0f66
KirilSnejko <164639799+KirilSnejko@users.noreply.github.com>
2024-08-01 16:59:37 +0300

    lesson-2-hw

--- lesson-2-hw/index.js
+++ lesson-2-hw/index.js
@@ -1,46 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-<script>
+// 1
 
+// function encrypt(string, key) {
+//   let encryptString = '';
+//   for (let i=0; i<string.length; i++) {
+//     const utf16 = string.charCodeAt(i) + key;
+//     const encryptedItem = String.fromCharCode(utf16)
+//     encryptString += encryptedItem;
+//   }
+//   return encryptString;
+// }
 
-  // 1 
-  
-  // function encrypt(string, key) {
-  //   let encryptString = '';
-  //   for (let i=0; i<string.length; i++) {
-  //     const utf16 = string.charCodeAt(i) + key;
-  //     const encryptedItem = String.fromCharCode(utf16)
-  //     encryptString += encryptedItem;
-  //   }
-  //   return encryptString;
-  // }
+// const encryptedString = encrypt('Hello, World!',3);
+// console.log(encryptedString);
 
-  // const encryptedString = encrypt('Hello, World!',3);
-  // console.log(encryptedString);
+// function decrypt(string, key) {
+//   let decryptedString = '';
+//   for (let i=0; i<string.length; i++) {
+//     const utf16 = string.charCodeAt(i) - key;
+//     const encryptedItem = String.fromCharCode(utf16)
+//     decryptedString += encryptedItem;
+//   }
+//   return decryptedString;
+// }
 
-  // function decrypt(string, key) {
-  //   let decryptedString = '';
-  //   for (let i=0; i<string.length; i++) {
-  //     const utf16 = string.charCodeAt(i) - key;
-  //     const encryptedItem = String.fromCharCode(utf16)
-  //     decryptedString += encryptedItem;
-  //   }
-  //   return decryptedString;
-  // }
+// const decryptedString = decrypt(encryptedString, 3)
+// console.log(decryptedString);
 
-  // const decryptedString = decrypt(encryptedString, 3)
-  // console.log(decryptedString);
-
-
-//   2 
+//   2
 
 // const encrypt = (str, key) => {
 //   let result = "";
@@ -79,7 +65,7 @@
 //   const letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 //   const symbols = "!@#$%^&*()_+";
 //   const allChars = numbers + letters + symbols;
-  
+
 //   let randomPassword = "";
 
 //   for (let i = 0; i < passwordLength; i++) {
@@ -90,7 +76,6 @@
 // }
 
 // generatePasswords(8)
-
 
 // 4
 
@@ -110,14 +95,13 @@
 //     let randomCharIndex = Math.floor(Math.random() * allChars.length);
 //     randomPassword += allChars[randomCharIndex];
 //   }
-  
+
 //   console.log(randomPassword);
 // }
 
-// generatePasswords(8, true, true, false); 
-// generatePasswords(12, false, true, true); 
+// generatePasswords(8, true, true, false);
+// generatePasswords(12, false, true, true);
 // generatePasswords(6, true, false, false);
-
 
 // 5
 
@@ -127,10 +111,3 @@
 // }
 
 // calculateString('3 + 5 * (2 - 8)')
-
-
-
-
-
-</script>
-</html>
